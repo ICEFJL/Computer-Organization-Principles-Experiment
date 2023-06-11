f3480e06d402ec9aee82e8101844253a6e738233
tracer-ics2017 <tracer@njuics.org>
2023-06-11 16:00:29 +0800

    >  compile 162130117 fujinlong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  16:00:28 up 5 min,  1 user,  load average: 0.17, 0.29, 0.17 e0648f0619ef1207a155247b2f13cf926714ea5f

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -63,7 +63,7 @@ make_EHelper(cltd)
 make_EHelper(cwtl)
 {
     if (decoding.is_operand_size_16) {
-        rtl_lr_b(&t0, R_AX);
+        rtl_lr_b(&t0, R_AL);
         rtl_sext(&t0, &t0, 1);
         rtl_sr_w(R_AX, &t0);
     } else {
