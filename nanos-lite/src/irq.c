853508ec99beb651211f432af21869bdf741a7df
tracer-ics2017 <tracer@njuics.org>
2023-06-16 15:01:58 +0800

    >  run 162130117 fujinlong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  15:01:58 up  5:42,  1 user,  load average: 0.84, 0.21, 0.07 6e8074073109f0e7bf203f8c6ac202784b80d6b7

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,13 +1,20 @@
 #include "common.h"
 
-static _RegSet* do_event(_Event e, _RegSet* r) {
-  switch (e.event) {
-    default: panic("Unhandled event ID = %d", e.event);
-  }
+_RegSet *do_syscall(_RegSet *r);
+static _RegSet *do_event(_Event e, _RegSet *r)
+{
+    switch (e.event) {
+        case _EVENT_SYSCALL:
+            do_syscall(r);
+            break;
+        default:
+            panic("Unhandled event ID = %d", e.event);
+    }
 
-  return NULL;
+    return NULL;
 }
 
-void init_irq(void) {
-  _asye_init(do_event);
+void init_irq(void)
+{
+    _asye_init(do_event);
 }
