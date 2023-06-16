560beb3be1d49f1d6ad2ef1fac0aed39e99db2f3
tracer-ics2017 <tracer@njuics.org>
2023-06-16 16:38:12 +0800

    >  run 162130117 fujinlong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  16:38:12 up  7:18,  1 user,  load average: 0.00, 0.00, 0.00 fdf808d2355540d616ed3a92b2682c53c26c5ca0

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,46 +4,60 @@
 
 int mm_brk(uint32_t new_brk);
 
-static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t mode) {
-  TODO();
-  return 1;
+static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t mode)
+{
+    TODO();
+    return 1;
 }
 
-static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  TODO();
-  return 1;
+static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len)
+{
+    return fs_write(fd, (void *)buf, len);
 }
 
-static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  TODO();
-  return 1;
+static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len)
+{
+    TODO();
+    return 1;
 }
 
-static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t whence) {
-  return fs_lseek(fd, offset, whence);
+static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t whence)
+{
+    return fs_lseek(fd, offset, whence);
 }
 
-static inline uintptr_t sys_close(uintptr_t fd) {
-  TODO();
-  return 1;
+static inline uintptr_t sys_close(uintptr_t fd)
+{
+    TODO();
+    return 1;
 }
 
-static inline uintptr_t sys_brk(uintptr_t new_brk) {
-  TODO();
-  return 1;
+static inline uintptr_t sys_brk(uintptr_t new_brk)
+{
+    TODO();
+    return 1;
 }
 
-_RegSet* do_syscall(_RegSet *r) {
-  uintptr_t a[4];
-  a[0] = SYSCALL_ARG1(r);
-  a[1] = SYSCALL_ARG2(r);
-  a[2] = SYSCALL_ARG3(r);
-  a[3] = SYSCALL_ARG4(r);
-  switch (a[0]) {
-    case SYS_none: r->eax = 1; break;
-    case SYS_exit: _halt(a[1]); break;
-    default: panic("Unhandled syscall ID = %d", a[0]);
-  }
+_RegSet *do_syscall(_RegSet *r)
+{
+    uintptr_t a[4];
+    a[0] = SYSCALL_ARG1(r);
+    a[1] = SYSCALL_ARG2(r);
+    a[2] = SYSCALL_ARG3(r);
+    a[3] = SYSCALL_ARG4(r);
+    switch (a[0]) {
+        case SYS_none:
+            r->eax = 1;
+            break;
+        case SYS_exit:
+            _halt(a[1]);
+            break;
+        case SYS_write:
+            r->eax = sys_write(a[1], a[2], a[3]);
+            break;
+        default:
+            panic("Unhandled syscall ID = %d", a[0]);
+    }
 
-  return NULL;
+    return NULL;
 }
